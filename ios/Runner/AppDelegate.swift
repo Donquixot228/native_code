f6e1cd4ef117999186e4b67d51997756eee07ec8
Donquixot228 <kirilnoskov402@gmail.com>
2022-10-18 18:43:43 +0300

    problem with appSwift\

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -12,8 +12,28 @@
  let batteryChannel = FlutterMethodChannel(name: "battery",
      binaryMessenger: self.flutterEngine.binaryMessenger)
 
-
- GeneratedPluginRegistrant.register(with: self)
- return super.application(application, didFinishLaunchingWithOptions: launchOptions)
- }
+    batteryChannel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: FlutterResult) -> Void in
+         switch call.method {
+           case "getBatteryLevel":
+             guard let args = call.arguments as? [String:String] else {return}
+             let name = args["name"]!
+             self.receiveBatteryLevel()
+           default: result(FlutterMethodNotImplemented)
+    }
+    })
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+   }
+     private func receiveBattery()->Int{
+         let device = UIDevice.current
+         device.isBatteryMonitoringEnabled=true
+         
+         if device.batteryState==UIDevice.BatteryState.unknown{
+             return -1
+         }
+        else{
+            return Int(device.batteryLevel*100)
+        }
+     }
  }
